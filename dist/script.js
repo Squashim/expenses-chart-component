05121756f920fb881d884d3bfcf448f9a39e094a
Squashim <konrad18v1@wp.pl>
2022-07-20 18:40:02 +0200

    75% done

--- dist/script.js
+++ dist/script.js
@@ -36,8 +36,39 @@ for(let i=0; i<chart.length; i++){
     daysName[i].textContent = chart[i].day;
     rows[i].style.height = (chart[i].amount * 300) / 100 + "px";
 }
+highlightToday()
 
+rows.forEach(row => {
+  row.addEventListener('click', () => {
+    const button = document.createElement("button");
+    button.textContent = chart[row.id - 1].amount;
+    row.parentElement.prepend(button);
+  }); //todo buttons design and on off validation
+});
 
-function heightCalc(){
-
+function highlightToday(){
+  const date = new Date();
+  const day = date.getDay();
+  switch (day){
+    case 1: 
+      rows[0].classList.add("today");
+      break;
+    case 2: 
+      rows[1].classList.add("today");
+      break;
+    case 3: 
+      rows[2].classList.add("today");
+      break;
+    case 4: 
+      rows[3].classList.add("today");
+      break;
+    case 5: 
+      rows[4].classList.add("today");
+      break;
+    case 6: 
+      rows[5].classList.add("today");
+      break;
+    case 7: 
+      rows[6].classList.add("today");
+  }
 }
