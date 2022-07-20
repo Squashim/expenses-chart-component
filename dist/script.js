9105886bd6c84ded2686bb877880fae3107cd107
Squashim <konrad18v1@wp.pl>
2022-07-20 15:07:16 +0200

    Initial

---
+++ dist/script.js
@@ -0,0 +1,41 @@
+const rows = Array.from(document.querySelectorAll(".table__day"));
+let chart = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
+
+for(let i=0; i<chart.length; i++){
+    rows[i].textContent = '<span class="rect">'+chart[i].day+'</span>';
+    rows[i].style.height = (chart[i].amount * 350) / 100 + "px";
+}
+
+
+function heightCalc(){
+
+}
